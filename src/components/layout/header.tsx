c28615eec0cf310ac0e0824df8b4b026ebfb0a1f
Luk Vermeulen <luklukas97@gmail.com>
2023-01-22 00:38:58 +0100

    underline link to current page

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,19 +1,34 @@
 import { Group, Text } from "@mantine/core";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export function Header() {
+  const router = useRouter();
+
   return (
     <Group>
       <Text href="/" component={Link}>
         /
       </Text>
-      <Text href="topology" component={Link}>
+      <Text
+        href="topology"
+        component={Link}
+        td={router.pathname === "/topology" ? "underline" : undefined}
+      >
         Toplogy
       </Text>
-      <Text href="representations" component={Link}>
+      <Text
+        href="representations"
+        component={Link}
+        td={router.pathname === "/representations" ? "underline" : undefined}
+      >
         Representations
       </Text>
-      <Text href="elements" component={Link}>
+      <Text
+        href="elements"
+        component={Link}
+        td={router.pathname === "/elements" ? "underline" : undefined}
+      >
         Elements
       </Text>
     </Group>
