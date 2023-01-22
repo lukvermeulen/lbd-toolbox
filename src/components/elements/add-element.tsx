490c938ce34d8505235900d47d9cce463ae9c50e
Luk Vermeulen <luklukas97@gmail.com>
2023-01-22 22:41:26 +0100

    add action prop

--- src/components/elements/add-element.tsx
+++ src/components/elements/add-element.tsx
@@ -1,11 +1,15 @@
 import { AspectRatio, Button, Card } from "@mantine/core";
 import { IconPlus } from "@tabler/icons";
 
-export function AddElement() {
+type AddElementProps = {
+  action?: () => void;
+};
+
+export function AddElement({ action }: AddElementProps) {
   return (
     <Card>
       <AspectRatio ratio={5 / 3} sx={{ maxWidth: 300 }} mx="auto">
-        <Button variant="outline">
+        <Button variant="outline" onClick={action}>
           <IconPlus />
         </Button>
       </AspectRatio>
