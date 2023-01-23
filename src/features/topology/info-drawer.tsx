d05761f1e16e42b84f3a41f1d209233701ca3ccc
Luk Vermeulen <luklukas97@gmail.com>
2023-01-23 13:16:04 +0100

    add InfoDrawer component

---
+++ src/features/topology/info-drawer.tsx
@@ -0,0 +1,22 @@
+import { Drawer } from "~/components/drawer/drawer";
+import { Stack, Text, Title } from "@mantine/core";
+
+type InfoDrawerProps = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  elementInfo: {
+    displayName: string;
+    name: string;
+  };
+};
+
+export function InfoDrawer({ open, setOpen, elementInfo }: InfoDrawerProps) {
+  return (
+    <Drawer open={open} setOpen={setOpen} title="Information">
+      <Stack>
+        <Title>{elementInfo.displayName}</Title>
+        <Text>IRI: {elementInfo.name}</Text>
+      </Stack>
+    </Drawer>
+  );
+}
--- src/features/topology/topology-element.tsx
+++ src/features/topology/topology-element.tsx
@@ -7,6 +7,7 @@ import {
   ActionIcon,
   Title,
   Tooltip,
+  Drawer,
 } from "@mantine/core";
 import {
   IconDots,
@@ -15,64 +16,75 @@ import {
   IconPencil,
   IconTrash,
 } from "@tabler/icons";
+import { useState } from "react";
+import { InfoDrawer } from "./info-drawer";
 
 type TopologyElementProps = {
   category: string;
   name: string;
 };
 export function TopologyElement({ category, name }: TopologyElementProps) {
+  const [infoOpen, setInfoOpen] = useState(false);
   const [id, ...displayName] = name.split("_");
+
   return (
-    <Card shadow={"xs"}>
-      <Stack>
-        {/* <AspectRatio ratio={5 / 3} sx={{ maxWidth: 300 }} mx="auto">
+    <>
+      <Card shadow={"xs"}>
+        <Stack>
+          {/* <AspectRatio ratio={5 / 3} sx={{ maxWidth: 300 }} mx="auto">
           </AspectRatio> */}
-        {/* <Image
+          {/* <Image
             src="https://images.unsplash.com/photo-1527118732049-c88155f2107c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
             alt="Panda"
           /> */}
-        <Group position="apart">
-          <Text>{category}</Text>
-          <Menu withinPortal position="bottom-start" shadow="sm">
-            <Menu.Target>
-              <ActionIcon>
-                <IconDots size={18} />
-              </ActionIcon>
-            </Menu.Target>
+          <Group position="apart">
+            <Text>{category}</Text>
+            <Menu withinPortal position="bottom-start" shadow="sm">
+              <Menu.Target>
+                <ActionIcon>
+                  <IconDots size={18} />
+                </ActionIcon>
+              </Menu.Target>
 
-            <Menu.Dropdown>
-              <Menu.Item icon={<IconPencil size={14} />}>Edit</Menu.Item>
-              <Menu.Item icon={<IconTrash size={14} />} color="red">
-                Delete
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-        </Group>
+              <Menu.Dropdown>
+                <Menu.Item icon={<IconPencil size={14} />}>Edit</Menu.Item>
+                <Menu.Item icon={<IconTrash size={14} />} color="red">
+                  Delete
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </Group>
 
-        <Group position="center">
-          <Tooltip label={id} withinPortal>
-            <Title order={3}>{displayName.join("_")}</Title>
-          </Tooltip>
-        </Group>
+          <Group position="center">
+            <Tooltip label={id} withinPortal>
+              <Title order={3}>{displayName.join("_")}</Title>
+            </Tooltip>
+          </Group>
 
-        <Group position="apart">
-          <ActionIcon>
-            <IconInfoCircle size={18} />
-          </ActionIcon>
+          <Group position="apart">
+            <ActionIcon onClick={() => setInfoOpen(true)}>
+              <IconInfoCircle size={18} />
+            </ActionIcon>
 
-          <Menu withinPortal position="bottom-start" shadow="sm">
-            <Menu.Target>
-              <ActionIcon>
-                <IconLine size={18} />
-              </ActionIcon>
-            </Menu.Target>
+            <Menu withinPortal position="bottom-start" shadow="sm">
+              <Menu.Target>
+                <ActionIcon>
+                  <IconLine size={18} />
+                </ActionIcon>
+              </Menu.Target>
 
-            <Menu.Dropdown>
-              <Menu.Item icon={<IconPencil size={14} />}>hasStorey</Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-        </Group>
-      </Stack>
-    </Card>
+              <Menu.Dropdown>
+                <Menu.Item icon={<IconPencil size={14} />}>hasStorey</Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </Group>
+        </Stack>
+      </Card>
+      <InfoDrawer
+        open={infoOpen}
+        setOpen={setInfoOpen}
+        elementInfo={{ displayName: displayName.join("_"), name: name }}
+      />
+    </>
   );
 }
