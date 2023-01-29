1201e80f423c3ff00e59025eec963a7cde39ea96
Luk Vermeulen <luklukas97@gmail.com>
2023-01-29 16:52:06 +0100

    save files in different folders
    - according to file type / representation type

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -12,20 +12,35 @@ export const config = {
 const post = async (req: NextApiRequest, res: NextApiResponse) => {
   const form = new formidable.IncomingForm();
   form.parse(req, async function (err, fields, files) {
-    const fileName = await saveFile(files.file);
+    let subPath: string;
 
-    console.log(fields);
-    return res
-      .status(201)
-      .json({ fileUrl: `http://localhost:3000/uploads/${fileName}` });
+    switch (fields.filetype) {
+      case "pointcloud":
+        subPath = "pointclouds";
+        break;
+      case "picture":
+        subPath = "pictures";
+        break;
+      default:
+        return res.status(500).json({ error: `Error handling file` });
+    }
+
+    const fileUrl = await saveFile(files.file, subPath);
+
+    return res.status(201).json({ fileUrl: fileUrl });
   });
 };
 
-const saveFile = async (file: FormidableFile | FormidableFile[]) => {
+const saveFile = async (
+  file: FormidableFile | FormidableFile[],
+  subPath: string
+) => {
   if (Array.isArray(file)) return false;
   const data = fs.readFileSync(file.filepath);
-  fs.writeFileSync(`./public/uploads/${file.originalFilename}`, data);
-  return file.originalFilename;
+  const filePath = `./public/uploads/${subPath}/${file.originalFilename}`;
+  const fileUrl = `http://localhost:3000/uploads/${subPath}/${file.originalFilename}`;
+  fs.writeFileSync(filePath, data);
+  return fileUrl;
 };
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
